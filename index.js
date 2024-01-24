4b05506142e16592ad5a4034fd4647026b9d0b7b
+ <+>
2024-01-23 22:05:34 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let p = document.getElementById('text');
+    p.textContent = "This is really cool!";
+   
+});
+
+
+
+
+
